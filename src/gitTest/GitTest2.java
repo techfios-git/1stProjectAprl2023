977696c5faca916f69bfd4f801e917159a16d555
techfios-git <shihabul.kh@gmail.com>
2023-10-07 13:44:03 -0500

    committed 3rd

---
+++ src/gitTest/GitTest2.java
@@ -0,0 +1,27 @@
+package gitTest;
+
+public class GitTest2 {
+	
+	public static void main(String[] args) {
+		printMe();
+		printMe1();
+		printMe2();
+	}
+	
+	public static void printMe() {
+		System.out.println("git test");
+	}
+	public static void printMe1() {
+		System.out.println("git test");
+	}
+	public static void printMe2() {
+		System.out.println("git test");
+	}
+	public static void printMe3() {
+		System.out.println("git test");
+	}
+	public static void printMe4() {
+		System.out.println("git test");
+	}
+
+}
