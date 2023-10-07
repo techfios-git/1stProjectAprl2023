269aeee93463c2b754c1b0d1472b46132ae253ac
techfios-git <shihabul.kh@gmail.com>
2023-10-07 11:37:22 -0500

    committed

---
+++ src/gitTest/Test1.java
@@ -0,0 +1,13 @@
+package gitTest;
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		printMe();
+	}
+	
+	public static void printMe() {
+		System.out.println("git test");
+	}
+
+}
